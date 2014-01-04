ac79e9224db10bb6b4d6ccbc85fda208bcce9ac7
Pang Yan Han <pangyanhan@gmail.com>
2014-01-04 16:22:34 +0800

    deploy.sh - deploy Haskell version of git-grep-filter
    and use build.sh for building git-grep-filter
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,28 @@
 # Copies the relevant scripts to $HOME/bin and sets their permissions
 
 BIN_DIR=$HOME/bin
+BuildSuccess=0
 
 case "$1" in
   all)
-    cp git-grep-blame.rb $BIN_DIR/ggb
-    cp git-grep-filter.rb $BIN_DIR/ggf
-    cp git_utils.rb $BIN_DIR/
-    cp setup_cabal.sh $BIN_DIR/setup_cabal
-    cp setup_haskell_sandboxes.rb $BIN_DIR/setup_haskell_sandboxes
-    cd $BIN_DIR
-    chmod 700 ggb ggf setup_cabal setup_haskell_sandboxes
+    ./build.sh git-grep-filter
+    BuildSuccess=$?
+    if [ $BuildSuccess -eq 0 ]
+    then
+      cp dist/build/git-grep-filter/git-grep-filter $BIN_DIR/ggf
+      cp git-grep-blame.rb $BIN_DIR/ggb
+      cp git_utils.rb $BIN_DIR/
+      cp setup_cabal.sh $BIN_DIR/setup_cabal
+      cp setup_haskell_sandboxes.rb $BIN_DIR/setup_haskell_sandboxes
+      cd $BIN_DIR
+      chmod 700 ggb ggf setup_cabal setup_haskell_sandboxes
+    else
+      echo "Deploy failed"
+    fi
     ;;
 
   git-grep-filter)
-    cabal build git-grep-filter
+    ./build.sh git-grep-filter
     if [ $? -eq 0 ]
     then
       cp dist/build/git-grep-filter/git-grep-filter $BIN_DIR/ggf
