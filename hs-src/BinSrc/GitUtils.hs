87eeda06441bffb4a8057c5117850afdf522791d
Pang Yan Han <pangyanhan@gmail.com>
2014-01-12 19:22:02 +0800

    GitUtils.truncated_colorized_line_io - Regex truncation
    This function makes use of the Data.Text.ICU.Regex module and the IO monad.
    Although we don't make use of the GitUtils.truncated_colorized_line_io in
    our code, it serves as an example of how to use the module and
    regular expressions in Haskell.
    
    Its code is also pretty messy due to hacking it out under heavy
    time constraint.
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- hs-src/BinSrc/GitUtils.hs
+++ hs-src/BinSrc/GitUtils.hs
@@ -2,10 +2,14 @@
 
 module BinSrc.GitUtils (
   truncate_colorized_line
+, truncate_colorized_line_io
 ) where
 
 import qualified Data.Text as T
-  ( Text, breakOn, concat, drop, length, null, take
+  ( Text, append, breakOn, concat, drop, length, null, take
+  )
+import qualified Data.Text.ICU.Regex as ICU
+  ( Regex, end, find, regex', setText, start
   )
 
 colorEscapeStart :: T.Text
@@ -55,3 +59,67 @@ truncate_colorized_line maxCols line =
                       coloredTextPartial = T.take takeLen coloredText
                   in
                     colorEscapeEnd : coloredTextPartial : textListWithColorStart
+
+truncate_colorized_line_io :: Int -> T.Text -> IO T.Text
+truncate_colorized_line_io maxCols line = do
+  eitherErrRegex <- ICU.regex' [] "\\e\\[[^m]*m\\e\\[K([^\\e]*)\\e\\[[^m]*m\\e\\[K"
+  case eitherErrRegex of
+    Left err -> do
+      putStrLn $ "regex parse error " ++ show err
+      return line
+    Right colorRegex -> do
+      ICU.setText colorRegex line
+      (takeTillLen, appendColorEscapeEnd) <- truncate_helper_io (0, 0) colorRegex
+      let truncatedLine = T.take takeTillLen line
+      if appendColorEscapeEnd
+        then
+          return $ T.append truncatedLine colorEscapeEnd
+        else
+          return truncatedLine
+
+  where
+    lineLen :: Int
+    lineLen = T.length line
+
+    truncate_helper_io :: (Int, Int) -> ICU.Regex -> IO (Int, Bool)
+    truncate_helper_io (startIdx, accumLen) colorRegex = do
+      foundPattern <- ICU.find colorRegex (fromIntegral startIdx)
+      if foundPattern
+        then do
+          mbI16GZeroStart <- ICU.start colorRegex 0
+          mbI16GZeroEnd <- ICU.end colorRegex 0
+          mbI16GOneStart <- ICU.start colorRegex 1
+          mbI16GOneEnd <- ICU.end colorRegex 1
+          case (mbI16GZeroStart, mbI16GZeroEnd, mbI16GOneStart, mbI16GOneEnd) of
+            (Just i16GZeroStart, Just i16GZeroEnd, Just i16GOneStart,
+             Just i16GOneEnd) ->
+              let gZeroStart = fromIntegral i16GZeroStart :: Int
+                  gZeroEnd = fromIntegral i16GZeroEnd :: Int
+                  gOneStart = fromIntegral i16GOneStart :: Int
+                  gOneEnd = fromIntegral i16GOneEnd :: Int
+                  coloredTextLen = gOneEnd - gOneStart
+                  prefixLen = gZeroStart - startIdx
+                  newAccumLen = accumLen + prefixLen
+                  finalAccumLen = newAccumLen + coloredTextLen
+              in
+                case compare newAccumLen maxCols of
+                  EQ -> return (gOneStart, False)
+                  GT -> return (startIdx + maxCols - accumLen, False)
+                  LT ->
+                    case compare finalAccumLen maxCols of
+                      LT -> truncate_helper_io (gZeroEnd, finalAccumLen) colorRegex
+                      EQ -> return (gZeroEnd, False)
+                      GT ->
+                        let nrColorCharsToTake = maxCols - newAccumLen
+                        in
+                          return (gOneStart + nrColorCharsToTake, True)
+
+            _ -> return (min lineLen maxCols, False)
+        else
+          if lineLen <= maxCols
+            then
+              return (lineLen, False)
+            else
+              let charsRemaining = maxCols - accumLen
+              in
+                return (startIdx + charsRemaining, False)
