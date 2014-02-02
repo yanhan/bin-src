33f9806620ee70f8f815450ebb9a0d1da372925e
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2014-02-02 20:47:23 +0800

    deploy.sh - update 'all' option
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -14,17 +14,18 @@ fi
 
 case "$1" in
   all)
-    ./build.sh git-grep-filter
+    ./build.sh all
     BuildSuccess=$?
     if [ $BuildSuccess -eq 0 ]
     then
       cp dist/build/git-grep-filter/git-grep-filter $BIN_DIR/ggf
+      cp dist/build/git-switch-branch/git-switch-branch $BIN_DIR/gsb
       cp git-grep-blame.rb $BIN_DIR/ggb
       cp git_utils.rb $BIN_DIR/
       cp setup_cabal.sh $BIN_DIR/setup_cabal
       cp setup_haskell_sandboxes.rb $BIN_DIR/setup_haskell_sandboxes
       cd $BIN_DIR
-      chmod 700 ggb ggf setup_cabal setup_haskell_sandboxes
+      chmod 700 ggb gsb ggf setup_cabal setup_haskell_sandboxes
     else
       echo "Deploy failed"
     fi
