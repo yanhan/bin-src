98b44b36897e805b059de804e0d0e645d488a849
Pang Yan Han <pangyanhan@gmail.com>
2014-03-06 22:02:11 +0800

    git-commit-time - suppress error messages from running git show command
    This is done by redirecting standard error to /dev/null
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- hs-src/git-commit-time.hs
+++ hs-src/git-commit-time.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Text (Text)
-import qualified Data.Text as T (pack, unpack)
+import qualified Data.Text as T (pack)
 import Shelly
-  ( Sh, liftIO, echo_err, errExit, exit, lastExitCode, run, shelly, silently
+  ( Sh, StdHandle(InHandle, OutHandle, ErrorHandle)
+  , StdStream(CreatePipe, Inherit, UseHandle), liftIO, echo_err, errExit, exit
+  , lastExitCode, runHandles, shelly, silently
   )
 import System.Environment (getArgs)
+import System.IO
+  ( Handle, IOMode(WriteMode), hClose, hGetContents, hPutStr, openFile
+  )
 import BinSrc.GitUtils (exit_if_not_in_git_repo)
 
 default (Text)
@@ -26,21 +31,41 @@ main = shelly $ silently $ do
           case mbCommitTime of
             Just commitTime ->
               putStr $ "commit " ++ commitSha1 ++ " was made on "
-                ++ T.unpack commitTime
+                ++ commitTime
             _ ->
               putStrLn $ commitSha1 ++ " is not a commit"
          ) $ zip cmdArgs commitTimeList
        )
   return ()
 
-get_commit_time_list :: [String] -> Sh [Maybe Text]
-get_commit_time_list commitSha1List =
-  mapM (\commitSha1 -> shelly $ errExit False $ silently $ do
-    outText <- run "git" ["show", "-s", "--format=%ci", T.pack commitSha1]
-    exitCode <- lastExitCode
-    if exitCode == 0
-      then
-        return $ Just outText
-      else
-        return Nothing
-  ) commitSha1List
+get_commit_time_list :: [String] -> Sh [Maybe String]
+get_commit_time_list commitSha1List = do
+  junkOutputHandle <- liftIO $ openFile "/dev/null" WriteMode
+  ret <- mapM (get_commit_time junkOutputHandle) commitSha1List
+  liftIO $ hClose junkOutputHandle
+  return ret
+  where
+    get_commit_time :: Handle -> String -> Sh (Maybe String)
+    get_commit_time junkOutputHandle commitSha1 =
+      shelly $ errExit False $ silently $ do
+        devNullHandle <- liftIO $ openFile "/dev/null" WriteMode
+        let devNullStdStream = UseHandle devNullHandle
+        outText <- runHandles "git" [ "show", "-s", "--format=%ci"
+                                    , T.pack commitSha1
+                                    ]
+          [ InHandle Inherit, OutHandle CreatePipe
+          , ErrorHandle devNullStdStream
+          ]
+          (\_ outHand _ -> do
+            t <- liftIO $ hGetContents outHand
+            -- force evaluation of hGetContents above
+            liftIO $ hPutStr junkOutputHandle t
+            return t
+          )
+        exitCode <- lastExitCode
+        liftIO $ hClose devNullHandle
+        if exitCode == 0
+          then
+            return $ Just outText
+          else do
+            return Nothing
