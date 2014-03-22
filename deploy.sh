78f77ee15f98fa8839d3bfa42ee541b40ef0a46a
Pang Yan Han <pangyanhan@gmail.com>
2014-03-22 23:16:53 +0800

    cljs-repl - enables Readline for ClojureScript REPL
    Based on https://github.com/emezeske/lein-cljsbuild/wiki/Using-Readline-with-REPLs-for-Better-Editing
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,7 @@ case "$1" in
     BuildSuccess=$?
     if [ $BuildSuccess -eq 0 ]
     then
+      cp cljs-repl.sh $BIN_DIR/cljs-repl
       cp dist/build/git-grep-filter/git-grep-filter $BIN_DIR/ggf
       cp dist/build/git-switch-branch/git-switch-branch $BIN_DIR/gsb
       cp git-grep-blame.rb $BIN_DIR/ggb
@@ -25,10 +26,15 @@ case "$1" in
       cp setup_cabal.sh $BIN_DIR/setup_cabal
       cp setup_haskell_sandboxes.rb $BIN_DIR/setup_haskell_sandboxes
       cd $BIN_DIR
-      chmod 700 ggb gsb ggf setup_cabal setup_haskell_sandboxes
+      chmod 700 cljs-repl ggb gsb ggf setup_cabal setup_haskell_sandboxes
     else
       echo "Deploy failed"
     fi
+    ;;
+
+  cljs-repl)
+    cp cljs-repl.sh $BIN_DIR/cljs-repl
+    chmod 700 $BIN_DIR/cljs-repl
     ;;
 
   git-grep-filter | ggf)
