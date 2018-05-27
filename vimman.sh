1ef3dfe655daaf7b0457724bb8bb1f84cdb25667
Pang Yan Han <pangyanhan@gmail.com>
2018-05-27 14:35:59 +0800

    [DOCS] vimman.sh: add 'about' and credit source of script
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- vimman.sh
+++ vimman.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# About: views manpage in vim
+# Source: https://twitter.com/nixcraft/status/973221066012229633?s=03
+
 main() {
 	MANPAGER="/bin/sh -c \"col -b | vim --not-a-term -c 'set ft=man ts=8 nomod nolist noma' -\""  man "$@"
 }
